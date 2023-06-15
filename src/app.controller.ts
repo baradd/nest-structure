849909c951db6c04d6129905685567da5b4b73e8
mohammad <mohammadtavassolian3@gmail.com>
2023-06-15 12:39:37 +0430

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,9 @@
+import { Get, Controller, Param } from "@nestjs/common"
+
+@Controller("/app")
+export class AppController {
+    @Get(":id")
+    async seyHello() {
+        return "Hi nest"
+    }
+}
